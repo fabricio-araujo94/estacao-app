34f77269b4740a7c0eb163e41f238c6d5443db1e
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2025-01-29 19:27:21 -0300

    background linear gradient

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,34 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, StyleSheet } from "react-native";
+import { ThemedView } from "@/components/ThemedView";
 
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Index() {
   return (
-    <View
+    <ThemedView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
+      <LinearGradient 
+        colors={['#FFFFFF', "#999999"]}
+        style={styles.background}
+      />
+
       <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    </ThemedView>
   );
 }
+
+
+const styles = StyleSheet.create({
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%',
+  },
+})
