3d976162933d43b6308eec23bc019cad35005287
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2025-02-04 18:00:43 -0300

    window and clothes hanging

--- app/index.tsx
+++ app/index.tsx
@@ -2,78 +2,84 @@ import { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { ThemedView } from "@/components/ThemedView";
 
-import Paho from 'paho-mqtt'
+import Paho from "paho-mqtt";
 
 import { LinearGradient } from "expo-linear-gradient";
 import { Image } from "expo-image";
 
 import { ButtonTouchable } from "@/components/ButtonTouchable";
 
-
 const client = new Paho.Client(
-  'broker.hivemq.com',
+  "broker.hivemq.com",
   Number(8000),
   `mqtt-async-test-${parseInt(String(Math.random() * 100).toString())}`
-)
+);
 
 export default function Index() {
-  const [temperature, setTemperature] = useState('38°C')
-  const [humidity, setHumidity] = useState('30%')
-  const [pressure, setPressure] = useState('5 atm')
-  const [rain, setRain] = useState('30%')
-  const [topic, setTopic] = useState('estacao')
-  
+  const [temperature, setTemperature] = useState("38°C");
+  const [humidity, setHumidity] = useState("30%");
+  const [pressure, setPressure] = useState("5 atm");
+  const [rain, setRain] = useState("30%");
+  const [window, setWindow] = useState(true);
+  const [clothesHanging, setClothesHanging] = useState(true);
+  const [topic, setTopic] = useState("estacao");
+
   function onMessage(newMessage: Paho.Message) {
-    if(newMessage.destinationName === 'estacao/temperature') {
-      setTemperature(newMessage.payloadString)
+    if (newMessage.destinationName === "estacao/temperature") {
+      setTemperature(newMessage.payloadString);
     }
 
-    if(newMessage.destinationName === 'estacao/humidity') {
-      setHumidity(newMessage.payloadString)
+    if (newMessage.destinationName === "estacao/humidity") {
+      setHumidity(newMessage.payloadString);
     }
 
-    if(newMessage.destinationName === 'estacao/pressure') {
-      setPressure(newMessage.payloadString)
+    if (newMessage.destinationName === "estacao/pressure") {
+      setPressure(newMessage.payloadString);
     }
 
-    if(newMessage.destinationName === 'estacao/rain') {
-      setRain(newMessage.payloadString)
+    if (newMessage.destinationName === "estacao/rain") {
+      setRain(newMessage.payloadString);
+    }
+
+    if (newMessage.destinationName === "estacao/window") {
+      setWindow(newMessage.payloadString === 'true');
+    }
+    
+    if (newMessage.destinationName === "estacao/clothesHanging") {
+      setClothesHanging(newMessage.payloadString === 'true');
     }
   }
 
-  function changeValue(c: Paho.Client) {
-    /*
-    const newMessage = new Paho.Message(message)
-    newMessage.destinationName = topic
-    c.send(newMessage)
-    */
+  function changeState(c: Paho.Client, topic: string, message: boolean) {
+    const newMessage = new Paho.Message(message.toString());
+    newMessage.destinationName = topic;
+    c.send(newMessage);
   }
 
   useEffect(() => {
     client.connect({
       onSuccess: () => {
-        console.log("Connected")
-        client.subscribe('estacao/#')
-        client.onMessageArrived = onMessage
+        console.log("Connected");
+        client.subscribe("estacao/#");
+        client.onMessageArrived = onMessage;
       },
       onFailure: () => {
-        console.log("Failed to connect")
-      }
-    })
-  }, [])
-  
-  
+        console.log("Failed to connect");
+      },
+    });
+  }, []);
+
   return (
     <ThemedView style={styles.container}>
-      <LinearGradient 
-        colors={['#FFFFFF', "#999999"]}
+      <LinearGradient
+        colors={["#FFFFFF", "#999999"]}
         style={styles.background}
       />
 
       <View style={styles.top}>
         <Image
           style={styles.image}
-          source={require('@/assets/images/cloud-rain.svg')}
+          source={require("@/assets/images/cloud-rain.svg")}
         />
         <Text style={styles.bigText}>{temperature}</Text>
       </View>
@@ -87,25 +93,25 @@ export default function Index() {
         <View style={styles.infoView}>
           <Image
             style={styles.icon}
-            source={require('@/assets/images/sea-drop.png')}
+            source={require("@/assets/images/sea-drop.png")}
           />
           <Text style={styles.infoNumber}>{humidity}</Text>
           <Text style={styles.infoLabel}>Umidade</Text>
         </View>
-        
+
         <View style={styles.infoView}>
           <Image
             style={styles.icon}
-            source={require('@/assets/images/barometer.png')}
+            source={require("@/assets/images/barometer.png")}
           />
           <Text style={styles.infoNumber}>{pressure}</Text>
           <Text style={styles.infoLabel}>Pressão Atmosférica</Text>
         </View>
-        
+
         <View style={styles.infoView}>
           <Image
             style={styles.icon}
-            source={require('@/assets/images/umbrella.png')}
+            source={require("@/assets/images/umbrella.png")}
           />
           <Text style={styles.infoNumber}>{rain}</Text>
           <Text style={styles.infoLabel}>Chuva</Text>
@@ -113,63 +119,61 @@ export default function Index() {
       </View>
 
       <View style={styles.inline}>
-        <ButtonTouchable
-          onClick={() => changeValue(client)}
-          icon='window'
-        />
+        <ButtonTouchable onClick={() => {
+          changeState(client, 'estacao/window', !window)
+          setWindow(!window)
+        }} icon="window" />
 
-        <ButtonTouchable
-          onClick={() => {}}
-          icon='clothes'
-        />
+        <ButtonTouchable onClick={() => {
+          changeState(client, 'estacao/clothesHanging', !clothesHanging)
+          setClothesHanging(!clothesHanging)
+        }} icon="clothes" />
       </View>
-
     </ThemedView>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    width: '100%',
-    height: '100%',
-    alignItems: 'center', 
-    justifyContent: 'center',
-    backgroundColor: '#fff',
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
   },
   background: {
-    position: 'absolute', 
+    position: "absolute",
     left: 0,
     right: 0,
     top: 0,
-    height: '100%',
+    height: "100%",
     zIndex: -1,
   },
   top: {
     width: 135,
     height: 172,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 40, 
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
   },
   top2: {
     width: 130,
     height: 41,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
-    marginBottom: 120
+    marginBottom: 120,
   },
   infoViewTop: {
-    flexDirection: 'row',
+    flexDirection: "row",
     width: 334,
     height: 107,
-    backgroundColor: '#D9D9D9',
+    backgroundColor: "#D9D9D9",
     borderRadius: 150,
-    borderColor: '#000000',
+    borderColor: "#000000",
     borderWidth: 0.5,
     elevation: 10, // Sombra para Android
-    shadowColor: '#000000', // Sombra para iOS
+    shadowColor: "#000000", // Sombra para iOS
     shadowOffset: { width: -2, height: 4 },
     shadowOpacity: 0.75,
     shadowRadius: 4,
@@ -178,15 +182,15 @@ const styles = StyleSheet.create({
   infoView: {
     flex: 1,
     width: 105,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   inline: {
     width: 273,
     height: 100,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 40, 
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 40,
   },
   image: {
     width: 122,
@@ -200,26 +204,26 @@ const styles = StyleSheet.create({
   bigText: {
     fontSize: 64,
     top: -16,
-    fontFamily: 'Itim',
+    fontFamily: "Itim",
   },
   local: {
     fontSize: 15,
-    fontFamily: 'Itim',
+    fontFamily: "Itim",
     opacity: 0.67,
   },
   hour: {
     fontSize: 24,
-    fontFamily: 'Caprasimo',
+    fontFamily: "Caprasimo",
     opacity: 0.53,
   },
   infoNumber: {
     fontSize: 16,
-    fontFamily: 'Itim',
+    fontFamily: "Itim",
   },
   infoLabel: {
     fontSize: 13,
-    fontFamily: 'Itim',
+    fontFamily: "Itim",
     opacity: 0.7,
-    textAlign: 'center',
+    textAlign: "center",
   },
-})
+});
