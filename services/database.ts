fa1985662f5466cae2b2ccf7b055244fe8cfaf22
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2025-02-10 23:02:39 -0300

    fix save to database

--- services/database.ts
+++ services/database.ts
@@ -2,50 +2,70 @@ import * as SQLite from "expo-sqlite";
 const db = SQLite.openDatabaseSync("estacao");
 
 type State = {
-  id: number,
-  timestampe: Date,
-  temperature: number,
-  humidity: number,
-  pressure: number,
-  rain: number
-}
+  id: number;
+  timestampe: Date;
+  temperature: number;
+  humidity: number;
+  pressure: number;
+  rain: number;
+};
 
 const initializeDB = () => {
-    db.execSync(`CREATE TABLE IF NOT EXISTS estacao (
+  db.execSync(`DROP TABLE estacao;`);
+
+  db.execSync(`CREATE TABLE IF NOT EXISTS estacao (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        timestamp DATETIME,
         temperature REAL,
         humidity REAL,
-        pressure REAL,
         rain REAL
       );`);
 
-    console.log("DB ok")
-  };
-
-const saveDataDB = async (temperature: string, humidity: string, pressure: string, rain: string) => {
-    const result = await db.runAsync(
-        "INSERT INTO estacao (temperature, humidity, pressure, rain) VALUES (?, ?, ?, ?)",
-        [temperature, humidity, pressure, rain]
-    );
-
-    console.log(result)
+  console.log("DB ok");
 };
 
-const deleteDataDB = async() => {
+const saveDataDB = async (
+  temperature: string,
+  humidity: string,
+  rain: string,
+  timestamp: string
+) => {
+  const date = new Date(timestamp);
+
   const result = await db.runAsync(
-    "DELETE FROM estacao",
+    "INSERT INTO estacao (temperature, humidity, rain, timestamp) VALUES (?, ?, ?, ?)",
+    [
+      temperature,
+      humidity,
+      rain,
+      date.toLocaleDateString("pt-BR", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+        hourCycle: "h23",
+      }),
+    ]
   );
 
-  console.log("dados deletados")
-} 
+  console.log(result.changes);
+};
+
+const deleteDataDB = async () => {
+  const result = await db.runAsync("DELETE FROM estacao");
+
+  console.log("dados deletados");
+};
 
 const getDataDB = async () => {
-    const allRows = await db.getAllAsync(`SELECT * FROM estacao`);
+  const allRows = await db.getAllAsync(`SELECT * FROM estacao`);
 
-    console.log(allRows)
+  console.log(allRows);
 
-    return allRows
-}
+  return allRows;
+};
 
-export { initializeDB, saveDataDB, getDataDB, deleteDataDB, State }
+export { initializeDB, saveDataDB, getDataDB, deleteDataDB, State };
